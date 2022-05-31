efae124bff5a2b111ed79ca304e1c132770e9ce5
James Parrish <jthomasparrishjr@gmail.com>
2022-05-31 14:39:03 -0500

    commit tree 8733064ce8f6c1a136e1be2524ae00a5f42508e8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(friends, string) {
+    let array = []
+for(let i = 0; i < friends.length; i++ ){
+    array.push(`Thank you, ${friends[i]}, for the wonderful ${string} gift!`)
+}
+return array
+}
+
+function countDown(x) {
+
+    while(x >= 0) {
+        console.log(x)
+        x--
+    }
+    
+
+}
